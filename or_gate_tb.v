7c35442474327ec6439cfe451a2b4c64e47e5a8f
samarthavkoushik <samarthavkoushik@gmail.com>
2026-07-11 20:30:43 +0530

    Add files via upload

---
+++ or_gate_tb.v
@@ -0,0 +1,41 @@
+`timescale 1ns/1ps
+
+module or_gate_tb;
+
+reg A;
+reg B;
+wire Y;
+
+or_gate uut 
+(
+    .A(A),
+    .B(B),
+    .Y(Y)
+);
+
+initial begin
+    $dumpfile("or_gate.vcd");
+    $dumpvars(0, or_gate_tb);
+    
+    $display("A B | Y");
+
+    A = 0; B = 0;
+    #10;
+    $display("%b %b | %b", A, B, Y);
+
+    A = 0; B = 1;
+    #10;
+    $display("%b %b | %b", A, B, Y);
+
+    A = 1; B = 0;
+    #10;
+    $display("%b %b | %b", A, B, Y);
+
+    A = 1; B = 1;
+    #10;
+    $display("%b %b | %b", A, B, Y);
+
+    $finish;
+end
+
+endmodule
