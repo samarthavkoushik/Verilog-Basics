238d28d8e423e75b3375ae74f616104e71bc864e
samarthavkoushik <samarthavkoushik@gmail.com>
2026-07-12 15:28:41 +0530

    Add files via upload

---
+++ nor_gate.v
@@ -0,0 +1,10 @@
+module nor_gate(
+    input A,
+    input B,
+    output Y
+);
+
+
+assign Y = ~ ( A | B );
+
+endmodule
---
+++ nor_gate_tb.v
@@ -0,0 +1,40 @@
+`timescale 1ns/1ps
+
+module nor_gate_tb;
+
+reg A;
+reg B;
+wire Y;
+
+nor_gate uut (
+    .A(A),
+    .B(B),
+    .Y(Y)
+);
+
+initial begin
+    $dumpfile("nor_gate.vcd");
+    $dumpvars(0, nor_gate_tb);
+    
+    $display("A B | Y ");
+
+    A = 0; B = 0;
+    #10;
+    $display("%b %b | %b", A, B, Y );
+
+    A = 0; B = 1;
+    #10;
+    $display("%b %b | %b", A, B, Y );
+
+    A = 1; B = 0;
+    #10;
+    $display("%b %b | %b ", A, B, Y );
+
+    A = 1; B = 1;
+    #10;
+    $display("%b %b | %b ", A, B, Y );
+
+    $finish;
+end
+
+endmodule
